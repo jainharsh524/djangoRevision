365f6e155ac958ec4b4f2edbc471b8097056efde
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-08 16:45:17 +0530

    learnt django templating

--- myApp/views.py
+++ myApp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hi, I am Harsh Jain. This is my first Django app")
+    context = {'lang':'Django',
+               'list':['Books', 'Pens', 'Vegies']
+               }
+    return render(request, 'home.html', context)
 
 def test(request, id, name):
     return HttpResponse(f"Hi, I am {name} and my I'd is {id}")
