9168d68cf9aac85a8e555303adb58bc92bffe94e
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-17 21:13:20 +0530

    learnt about models

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Student_Profile
+
+admin.site.register(Student_Profile)
 
 # Register your models here.
--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+import pickle
 # Create your models here.
+class Student_Profile(models.Model):
+    name = models.CharField(max_length=200)
+    roll_no = models.CharField(max_length=40)
+    branch = models.CharField(max_length=40)
+    marks = models.IntegerField()
+    photo = models.ImageField()
+
+    def __str__(self):
+        return f"{self.name} - {self.roll_no}"
